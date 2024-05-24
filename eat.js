eae084d7bccc12b92c3d21fcdeb94ccd6bef2739
rhapsodydevv <yvettenjeri79@gmail.com>
2024-05-24 14:44:40 +0300

    Began!

---
+++ eat.js
@@ -0,0 +1,11 @@
+let fruits = ["banana", "apple", "watermelon", "pineapple", "orange", "mango"]
+let answer = document.getElementById('answer').innerHTML
+
+
+let randomF = Math.floor(Math.random()*fruits.length)
+
+function random(){
+    
+}
+
+console.log(typeof(fruits[randomF]))
