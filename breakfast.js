62ea8c22eb9c39dda6529cf2f36802266b5fafaa
rhapsodydevv <yvettenjeri79@gmail.com>
2024-05-25 17:15:06 +0300

    Almost done!

--- breakfast.js
+++ breakfast.js
@@ -1,12 +1,170 @@
 function randomizeB(){
-    let breakfastOptions = ['Cornflakes', 'Eggs', 'Bacon', 'Bread']
-    let randomOption = breakfastOptions[Math.floor(Math.random()*breakfastOptions.length)]
+    let select = document.getElementById('breakfast')
+    let options = select.children
+    let randomOption = Math.floor(Math.random()*options.length)
+    select.value = options[randomOption].value
     let selectedd = document.getElementById('selectedB')
-    selectedd.innerText = randomOption
+    selectedd.innerText = select.value
+    let imageUrl = ''
+    switch(select.value){
+        case 'Cornflakes':
+            imageUrl = 'url(cornflakes.jpg)';
+            break;
+        case 'Eggs':
+            imageUrl = 'url(eggs.jpg)'
+            break;
+        case 'Bacon':
+            imageUrl = 'url(bacon.jpg)'
+            break;
+        case 'Bread':
+            imageUrl = 'url(Bread.jpg)'
+            break;
+        case 'Pancake':
+            imageUrl = 'url(pancake.jpg)'
+            break;
+        default:
+            imageUrl = '';
+        
+    }
+    selectedd.style.backgroundImage = imageUrl
+    
 }
+
+
 function selectedOptionB(){
     let selectElement = document.getElementById('breakfast')
     let selectedValue = selectElement.value
     let selectedd = document.getElementById('selectedB')
     selectedd.innerText = selectedValue
+    let imageUrl = ''
+    switch(selectedValue){
+        case 'Cornflakes':
+            imageUrl = 'url(cornflakes.jpg)';
+            break;
+        case 'Eggs':
+            imageUrl = 'url(eggs.jpg)'
+            break;
+        case 'Bacon':
+            imageUrl = 'url(bacon.jpg)'
+            break;
+        case 'Bread':
+            imageUrl = 'url(Bread.jpg)'
+            break;
+        case 'Pancake':
+            imageUrl = 'url(pancake.jpg)'
+            break;
+        default:
+            imageUrl = '';
+        
+    }
+    selectedd.style.backgroundImage = imageUrl
+}
+
+function randomizeS(){
+    let select = document.getElementById('snacks')
+    let options = select.children
+    let randomOption = Math.floor(Math.random()*options.length)
+    select.value = options[randomOption].value
+    let selectedd = document.getElementById('selectedS')
+    selectedd.innerText = select.value
+    let imageUrl = '';
+    switch(select.value){
+        case 'Apple':
+            imageUrl = 'url(apple.jpg)';
+            break;
+        case 'Avocado':
+            imageUrl = 'url(avocado.jpg)';
+            break;
+        case 'Banana':
+            imageUrl = 'url(banana.jpg)';
+            break;
+        case 'Cake':
+            imageUrl = 'url(cake.jpg)';
+            break;
+        case 'Cookies':
+            imageUrl = 'url(cookies.jpg)';
+            break;
+        case 'Crisps':
+            imageUrl = 'url(crisps.jpg)';
+            break;
+        case 'Cupcake':
+            imageUrl = 'url(cupcakes.jpg)';
+            break;
+        case 'Grapes':
+            imageUrl = 'url(grapes.jpg)';
+            break;
+        case 'Mango':
+            imageUrl = 'url(mango.jpg)';
+            break;
+        case 'Nuts':
+            imageUrl = 'url(nuts.jpg)';
+            break;
+        case 'Orange':
+            imageUrl = 'url(orange.jpg)';
+            break;
+        case 'Pineapple':
+            imageUrl = 'url(pineapple.jpg)';
+            break;
+        case 'Watermelon':
+            imageUrl = 'url(watermelon.jpg)';
+            break;
+
+        default:
+            imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
+}
+
+
+function selectedOption(){
+    let selectElement = document.getElementById('snacks')
+    let selectedValue = selectElement.value
+    let selectedd = document.getElementById('selectedS')
+    selectedd.innerText = selectedValue
+    let imageUrl = '';
+    switch(selectedValue){
+        case 'Apple':
+            imageUrl = 'url(apple.jpg)';
+            break;
+        case 'Avocado':
+            imageUrl = 'url(avocado.jpg)';
+            break;
+        case 'Banana':
+            imageUrl = 'url(banana.jpg)';
+            break;
+        case 'Cake':
+            imageUrl = 'url(cake.jpg)';
+            break;
+        case 'Cookies':
+            imageUrl = 'url(cookies.jpg)';
+            break;
+        case 'Crisps':
+            imageUrl = 'url(crisps.jpg)';
+            break;
+        case 'Cupcake':
+            imageUrl = 'url(cupcakes.jpg)';
+            break;
+        case 'Grapes':
+            imageUrl = 'url(grapes.jpg)';
+            break;
+        case 'Mango':
+            imageUrl = 'url(mango.jpg)';
+            break;
+        case 'Nuts':
+            imageUrl = 'url(nuts.jpg)';
+            break;
+        case 'Orange':
+            imageUrl = 'url(orange.jpg)';
+            break;
+        case 'Pineapple':
+            imageUrl = 'url(pineapple.jpg)';
+            break;
+        case 'Watermelon':
+            imageUrl = 'url(watermelon.jpg)';
+            break;
+
+        default:
+            imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
--- lunch.js
+++ lunch.js
@@ -1,37 +1,197 @@
 function randomizeC(){
-    let lunchOptions = ['Rice', 'Ugali', 'Spaghetti', 'Pasta']
-    let randomOption = lunchOptions[Math.floor(Math.random()*lunchOptions.length)]
+    let select = document.getElementById('carbohydrates')
+    let options = select.children
+    let randomOption = Math.floor(Math.random()*options.length)
+    select.value = options[randomOption].value
     let selectedd = document.getElementById('selectedC')
-    selectedd.innerText = randomOption
+    selectedd.innerText = select.value
+    let imageUrl = '';
+    switch(select.value){
+        case 'Rice':
+            imageUrl = 'url(rice.jpg)';
+            break;
+        case 'Ugali':
+            imageUrl = 'url(ugali.jpg)';
+            break;
+        case 'Spaghetti':
+            imageUrl = 'url(spaghetti.jpg)';
+            break;
+        case 'Pasta':
+            imageUrl = 'url(pasta.jpg)';
+            break;
+        case 'Potatoes':
+            imageUrl = 'url(potatoes.jpg)';
+            break;
+        case 'Chapatti':
+            imageUrl = 'url(chapati.jpg)';
+            break;
+
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
 function selectedOptionC(){
     let selectElement = document.getElementById('carbohydrates')
     let selectedValue = selectElement.value
     let selectedd = document.getElementById('selectedC')
     selectedd.innerText = selectedValue
+    let imageUrl = '';
+    switch(selectedValue){
+        case 'Rice':
+            imageUrl = 'url(rice.jpg)';
+            break;
+        case 'Ugali':
+            imageUrl = 'url(ugali.jpg)';
+            break;
+        case 'Spaghetti':
+            imageUrl = 'url(spaghetti.jpg)';
+            break;
+        case 'Pasta':
+            imageUrl = 'url(pasta.jpg)';
+            break;
+        case 'Potatoes':
+            imageUrl = 'url(potatoes.jpg)';
+            break;
+        case 'Chapatti':
+            imageUrl = 'url(chapati.jpg)';
+            break;
+
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
+
+
 function randomizeP(){
-    let lunchOptions = ['Beef', 'Fish', 'Lentils', 'Greengrams', 'Goatmeat', 'Beans']
-    let randomOption = lunchOptions[Math.floor(Math.random()*lunchOptions.length)]
+    let select = document.getElementById('protein')
+    let options = select.children
+    let randomOption = Math.floor(Math.random()*options.length)
+    select.value = options[randomOption].value
     let selectedd = document.getElementById('selectedP')
-    selectedd.innerText = randomOption
+    selectedd.innerText = select.value
+    let imageUrl = '';
+    switch(select.value){
+        case 'Beef':
+            imageUrl = 'url(beef.jpg)';
+            break;
+        case 'Fish':
+            imageUrl = 'url(fish.jpg)';
+            break;
+        case 'Beans':
+            imageUrl = 'url(beans.jpg)';
+            break;
+        case 'Goat':
+            imageUrl = 'url(beef.jpg)';
+            break;
+        case 'Lentils':
+            imageUrl = 'url(lentils.jpg)';
+            break;
+        case 'Greengrams':
+            imageUrl = 'url(lentils.jpg)';
+            break;
+        case 'Chicken':
+            imageUrl = 'url(chicken.jpg)';
+            break;
+            
+
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
 function selectedOptionP(){
     let selectElement = document.getElementById('protein')
     let selectedValue = selectElement.value
     let selectedd = document.getElementById('selectedP')
     selectedd.innerText = selectedValue
+    let imageUrl = '';
+    switch(selectedValue){
+        case 'Beef':
+            imageUrl = 'url(beef.jpg)';
+            break;
+        case 'Fish':
+            imageUrl = 'url(fish.jpg)';
+            break;
+        case 'Beans':
+            imageUrl = 'url(beans.jpg)';
+            break;
+        case 'Goat':
+            imageUrl = 'url(beef.jpg)';
+            break;
+        case 'Lentils':
+            imageUrl = 'url(lentils.jpg)';
+            break;
+        case 'Greengrams':
+            imageUrl = 'url(lentils.jpg)';
+            break;
+        case 'Chicken':
+            imageUrl = 'url(chicken.jpg)';
+            break;
+            
+
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
+
+
 function randomizeV(){
-    let lunchOptions = ['Spinach', 'Kales', 'Cabbage', 'Terere']
-    let randomOption = lunchOptions[Math.floor(Math.random()*lunchOptions.length)]
+    let select = document.getElementById('vitamins')
+    let options = select.children
+    let randomOption = Math.floor(Math.random()*options.length)
+    select.value = options[randomOption].value
     let selectedd = document.getElementById('selectedV')
-    selectedd.innerText = randomOption
+    selectedd.innerText = select.value
+    let imageUrl = '';
+    switch(select.value){
+        case 'Cabbage':
+            imageUrl = 'url(cabbage.jpg)';
+            break;
+        case 'Veggies':
+            imageUrl = 'url(veggies.jpg)';
+            break;
+        case 'Spinach':
+            imageUrl = 'url(greens.jpg)';
+            break;
+        case 'Kales':
+            imageUrl = 'url(greens.jpg)';
+            break;
+        case 'Terere':
+            imageUrl = 'url(greens.jpg)';
+            break;         
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
 function selectedOptionV(){
     let selectElement = document.getElementById('vitamins')
     let selectedValue = selectElement.value
     let selectedd = document.getElementById('selectedV')
     selectedd.innerText = selectedValue
+    let imageUrl = '';
+    switch(selectedValue){
+        case 'Cabbage':
+            imageUrl = 'url(cabbage.jpg)';
+            break;
+        case 'Veggies':
+            imageUrl = 'url(veggies.jpg)';
+            break;
+        case 'Spinach':
+            imageUrl = 'url(greens.jpg)';
+            break;
+        case 'Kales':
+            imageUrl = 'url(greens.jpg)';
+            break;
+        case 'Terere':
+            imageUrl = 'url(greens.jpg)';
+            break;         
+        default:
+                imageUrl = '';
+    }
+    selectedd.style.backgroundImage = imageUrl
 }
 
